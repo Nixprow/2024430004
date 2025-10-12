b1420a6d589cc4b13023f89bd5b3f4b623910454
Nicolás Valdivia <nvaldivia470@gmail.com>
2025-10-12 19:28:38 -0300

    Se añade actividad 4 con un ejemplo de grafo que hice ejecutando el programa

--- Valdivia_Nicolas_Actividad_1.cpp
+++ Valdivia_Nicolas_Actividad_1.cpp
@@ -1,236 +1,295 @@
 #include <iostream>
-#include <string>
+#include <fstream>
+#include <cstdlib>
 using namespace std;
 
-//Gran parte del codigo, se guia en la ayuda del docente, el archivo pila_con_nodo.cpp
 struct Nodo{
-    string nombre; //Atributos propios de la clase estudiante solo que se agrupan dentro de una estructura llamada Nodo
-    string carrera;
-    float promedio;
-    Nodo* siguiente;
+    int info;
+    Nodo* izq; //Este puntero apunta hacia la rama izquierda
+    Nodo* der; //Este puntero apunta hacia la rama derecha
 
 };
 
-struct Cola{
-    Nodo* frente; //Esta variable marca un puntero al inicio de la lista dinamica
-    Nodo* final;//Y esta marca el ultimo
-
-};
-
-void inicializarCola(Cola*& cola){
-
-    cola= new Cola(); //Esta linea crea una nueva estructura Cola en memoria dinamica
-    cola->frente = nullptr; // Esta linea se encarga de comenzar el frente como nulo
-    cola->final=nullptr;//Y el final tambien
-
+//La siguiente funcion tiene por proposito crear un nuevo nodo
+Nodo* crearNodo (int data){
+    Nodo* nuevo = new Nodo; //Esta linea reserva memoria para el nuevo nodo a crear
+    nuevo ->info= data; //Se le asigna el valor al nodo
+    //Luego se inicializan los 2 hijos, como nulos, ya que no hay nada en esa ubicacion en el momento
+    nuevo->izq = nullptr;
+    nuevo ->der = nullptr;
+    return nuevo; // Y luego se retorna el nuevo nodo creado
 }
 
-bool colaVacia(Cola* cola){
-
-    return(cola->frente==nullptr); //Esta linea o más bien la funcion se encarga de verificar si la cola está vacia, y si frente es igual a nullptr, significa que si se cumple
-}
-//Lo que hará esta funcion será insertar un estudiante al final de la cola 
-void insertarCola(Cola* cola,string nombre,string carrera,float promedio){
-    
-    //Se crea un nuevo nodo en memoria dinamica
-    Nodo* nuevo = new Nodo();
-    //Se crean los atributos de la clase 
-    nuevo->nombre = nombre;
-    nuevo->carrera= carrera;
-    nuevo->promedio = promedio;
-    nuevo-> siguiente = nullptr; //Esté representa el ultimo por lo cual se le asigna el nulo
-
-    //Se usa el if para ver si la cola está vacia
-    if (colaVacia(cola)){
-        //Y si esta cola se encuentra vacia lo que hará es que el nodo nuevo, se encuentre tanto al inicio como al final
-        cola->frente = nuevo;
-        cola->final= nuevo;
-    } else {
-        //Pero en caso de que no esté vacio se enlaza el ultimo nodo al nuevo
-        cola->final->siguiente= nuevo;
-        cola-> final=nuevo;//Actualiza el final para q el final sea el nuevo nodo
-
+bool buscarValor(Nodo* nodo, int valor){
+    if (nodo == nullptr){ //Si llegamos a nulo, retorna falso
+        return false;
+    }
+    if (nodo-> info== valor){ //Si encontramos el valor que buscamos retorna a true
+        return true;
     }
 
-    cout<<"Estudiante: "<<nombre<<" agregado a la cola. "<<endl;
-
-
+    if (valor < nodo->info){
+        return buscarValor(nodo ->izq, valor); //Si es menor se busca en izquierda y si no lo hará en derecha como la linea de más abajo
+    }
+    return buscarValor(nodo ->der,valor);
 
 }
 
-bool eliminarCola(Cola* cola, string& nombre,string& carrera,float& promedio){
-
-    //Primero se verifica si la cola está vacia o no
-    if (colaVacia(cola)){
-        return false; //No se puede eliminar cola si está se encuentra vacia
-
+Nodo* insertarNum (Nodo* nodo, int valor){
+    if (nodo==nullptr){ //Si la posicion está vacia se crea un nuevo nodo
+        return crearNodo(valor); 
     }
-    //Guardar referencia al nodo que se va a eliminar, el cual se le pone de nombre auxiliar
-    Nodo* aux = cola->frente;
 
-    //Esto extrae los datos del nodo antes de eliminarlo
-    nombre=aux->nombre;
-    carrera= aux->carrera;
-    promedio = aux->promedio;
-    
-    //Verifica si solo hay un elemento en la cola
-    if (cola->frente == cola ->final){
-        //Y si solo hay uno la cola queda vacia 
-        cola->frente = nullptr;
-        cola->final = nullptr;
+    if(valor<nodo->info){   //Este if plantea que si el valor es menor al nodo actual, se inserta en el subarbol izq
+        nodo->izq = insertarNum(nodo->izq,valor);
+
+    } else if (valor > nodo->info){ //Este es en caso de que sea mayor que el nodo actual, y por lo tanto se inserta en el subarbol derecho
+        nodo->der = insertarNum(nodo ->der,valor); 
 
     } else {
+        cout<<"El numero "<<valor<< " ya existe en el árbol. No se permiten dupllicados"<<endl;
 
-        cola->frente= cola->frente->siguiente;
     }
 
-    delete aux;
-    return true;
+    return nodo; 
 
+}
+//Este busca al nodo minimo que seria a la izquiera ya que va de menor a mayor
+Nodo* encontrarMinimo(Nodo* nodo){
+    while (nodo->izq != nullptr){
+        nodo = nodo->izq;
+    }
+    return nodo;
 
 }
 
-void mostrarCola(Cola* cola){
-    if (colaVacia(cola)){
-        cout<<"la cola está vacia."<<endl;
+Nodo* removerNum(Nodo* nodo, int valor){
+    if(nodo == nullptr){ //Si el arbol está vacio da el mensaje de que no está como para eliminarlo
+        cout<<"El numero "<<valor<< " no se encuentra en el arbol"<<endl;
+        return nodo;
+
+    }
+
+    if (valor < nodo->info){
+        nodo->izq = removerNum(nodo->izq,valor); //Busca por la izquierda
+    } else if (valor > nodo->info){
+        nodo ->der= removerNum(nodo->der,valor); //Busca en la derecha
+
+    } else {
+        //Si es que se encuentra el nodo, tenemos 3 posibles casos
+
+
+        //Primer caso: Nodo hoja
+        if (nodo ->izq == nullptr){ //ESte if verifica si solo tiene un hijo derecho o si es hoja
+            Nodo* temp = nodo->der;
+            delete nodo;
+            cout<<"Numero "<<valor<<" eliminado correctamente"<<endl;
+            return temp;
+        } else if (nodo-> der == nullptr){ //Este if es en caso de que solo tenga al hijo izq
+            Nodo* temp= nodo->izq; //Se guarda la referencia del hijo
+            delete nodo;
+            cout<<"Numero "<<valor<< " eliminado correctamente"<<endl;
+            return temp; //Conecta el hijo al padre
+
+        }
+
+        //Segundo caso: Nodo con 2 hijos
+        Nodo* temp= encontrarMinimo(nodo->der); //Esta linea encuentra el minimo del subarbol derecho
+
+        nodo->info = temp->info;
+        nodo->der = removerNum(nodo->der,temp->info); //Elimina el minimo original
+
+    }
+
+    return nodo;
+}
+
+//Esta funcion es para modificar un elemento, valorA es valor antigo y valor N es valor nuevo porsiacaso
+Nodo* modificar(Nodo* raiz, int valorA, int valorN){
+    if (buscarValor(raiz,valorN)){ //Verificar si el valor nuevo ya existe
+        cout<<"El nuevo valor "<< valorN<<" ya existe en el arbol"<<endl;
+
+        return raiz;
+    }
+
+    raiz= removerNum(raiz,valorA); //Se elimina el valor antiguo y se inserta el nuevo xd
+    raiz= insertarNum(raiz,valorN);
+
+    cout<<"Valor modificado de "<< valorA <<" a "<<valorN<<" correctamente."<<endl;
+    return raiz;
+}   
+//Esta funcion debe partir por la raiz para luego ir a la izquierda y luego derecha
+void preOrden(Nodo* nodo){
+    if (nodo==nullptr){ //Esto es en caso de nodo nulo o la base por decirlo asi
+        return;
+    }
+    cout<<nodo->info<< " "; //Asi se visita la raiz y se imprime
+    //Recorre lado izquierdo y luego el derecho
+    
+    preOrden(nodo->izq);
+    preOrden(nodo->der);
+}
+//Esta funcion parte por la izquierda, llega a la raiz y luego a la rama derecha
+void inOrden(Nodo* nodo){
+    if(nodo== nullptr){
+        return;
+    }
+    inOrden(nodo->izq);
+    cout<< nodo->info<< " "; //Visita raiz
+    inOrden(nodo->der);
+}
+//Esta funcion parte por la izquierda-derecha y al final la raiz
+void posOrden(Nodo* nodo){
+    if (nodo==nullptr){
+        return;
+    }
+    posOrden(nodo->izq);
+    posOrden(nodo->der);
+    cout<<nodo->info<<" ";
+}
+//Esta funcion crea con Graphviz
+void generarGraphviz(Nodo* nodo, ofstream& fp){
+    if (nodo != nullptr){
+        //Primero se procesa el hijo izq
+        if (nodo->izq!=nullptr){
+            //En caso de que este exista se crea una conexion
+            fp<<"    "<<nodo->info<<"->"<<nodo->izq->info<<";"<<endl;
+        } else {
+            //Si no existe se crea el nodo fantasma (ese punto q marca q en realidad no hay na)
+            string nodoNulo = "null" + to_string(nodo->info)+ "l";
+            fp <<"    "<<nodoNulo<<" [shape=point];"<<endl;
+            fp <<"    "<<nodo->info<<"->"<<nodoNulo<<";"<<endl;
+        }
+
+        if (nodo->der!=nullptr){
+            //En caso de que este exista se crea una conexion
+            fp<<"    "<<nodo->info<<"->"<<nodo->der->info<<";"<<endl;
+        } else {
+            //Si no existe se crea el nodo fantasma (ese punto q marca q en realidad no hay na)
+            string nodoNulo = "null" + to_string(nodo->info)+ "r";
+            fp <<"    "<<nodoNulo<<" [shape=point];"<<endl;
+            fp <<"    "<<nodo->info<<"->"<<nodoNulo<<";"<<endl;
+        }
+
+        generarGraphviz(nodo->izq, fp);
+        generarGraphviz(nodo->der, fp);
+    }
+}
+//Funcion para crear el grafo
+void generarGrafo(Nodo* raiz) {
+    ofstream fp("grafo.txt");  // Abre archivo para escritura
+    
+    if (!fp.is_open()) {
+        cerr << "Error al crear el archivo grafo.txt" << endl;
+        return;
+    }
+    
+    // Escribi encabezado del archivo DOT
+    fp << "digraph G {" << endl;
+    fp << "    node [style=filled fillcolor=yellow];" << endl;
+    
+    // Genera contenido del árbol
+    generarGraphviz(raiz, fp);
+    
+    fp << "}" << endl;  // Cerra el grafo
+    fp.close();         // Cerra archivo
+    
+    // Ejecuta Graphviz para generar imagen PNG
+    system("dot -Tpng -ografo.png grafo.txt");
+    cout << "Archivo grafo.png generado correctamente.\n";
+}
+//Con el comando eog grafo.png se puede visualizar el grafo
+//y claro el "grafo" de grafo.png se reemplazaria por el nombre del png a visualizar
+//Esta funcion es la de liberar memoria pero aplicada al arbol y esta se encarga de eliminar primero el subarbol izquierdo y luego el derecho
+void liberarMemoria(Nodo* nodo){
+    if (nodo==nullptr){
         return;
     }
 
-    cout<<"\n---CANTIDAD ACTUAL DE LA COLA---\n";
-    int pos = 1;
-    Nodo* actual = cola ->frente;
-    while (actual != nullptr){
-        cout<< pos << "- "<<actual->nombre<<" | "
-            <<actual->carrera <<" | Promedio: "
-            <<actual -> promedio<<endl;
-        actual = actual->siguiente;
-        pos++;
-    }
-    cout<<"----------------------------------\n";
-    
+    liberarMemoria(nodo->izq);
+    liberarMemoria(nodo->der);
+    delete nodo;
 }
-//Esta borra una posicion en una posicion especifica, lo cual es distinto del eliminar cola, que este sera el dequeue
-bool borrarPosicion(Cola* cola, int posicion){
-
-    if ((colaVacia(cola)) || posicion < 1){
-        return false;
-    }
-    //Este if es para un caso especial y se encarga de borrar el primer elemento o el frente de la cola
-    if (posicion == 1){
-        string nombre;
-        string carrera;
-        float promedio;
-        return eliminarCola(cola,nombre,carrera,promedio);//En este caso se reutiliza la funcion existente de eliminar cola
-    }
-
-    Nodo* actual=cola->frente; // Empieza desde el frente
-    Nodo* anterior = nullptr;// Guardar referencia al nodo anterior
-    int contador= 1;
-
-    //Avanzar hasta llegar a la posicion deseada
-    while (actual != nullptr && contador < posicion){
-        anterior = actual;  //Guarda el nodo anterior
-        actual = actual -> siguiente; // Avanza al siguiente nodo
-        contador++;  // esto hace que el contado avance
-    }
-    if (actual == nullptr){
-        return false;
-    }
-
-    anterior ->siguiente = actual->siguiente; //Lo que hace la siguiente linea es saltar al nodo que busca eliminar
-
-    //Si se eliminó el ultimo elemento, reorganiza el final
-    if (actual == cola->final){
-        cola->final = anterior; //El anterior ahora es el ultimo
-
-    }
-
-    //Libera la memoria del nodo eliminado
-    delete actual;
-    return true;
-    
-}
-
-
 int main(){
-    Cola* cola;
-    inicializarCola(cola); //Se inicializa la cola vacia llamando a la funcion de inicializarCola
-    //He de aclarar que es primera vez que trabajo con switch, por lo cual m cuesta algo aun
-    int opcion;
-    string nombre,carrera;
-    float promedio;  
-    int pos;    
-    
-    //Se hace uso del do y de switch, para trabajar con el menu
 
-    do { 
-        cout<<"\n---MENU ESTUDIANTES COLA---\n";
-        cout<<"1-Insertar estudiante"<<endl;
-        cout<<"2-Eliminar estudiante"<<endl;
-        cout<<"3-Mostrar cola completa"<<endl;
-        cout<<"4-Borrar una posicion especifica"<<endl;
-        cout<<"5-Verificar si la cola está vacia"<<endl;
-        cout<<"6-Salir"<<endl;
-        cout<<"Seleccione una opcion: "<<endl;
+    Nodo* raiz =nullptr; //La raiz del arbol o el primer nodo se incializa como vacio
+    int opcion, valor,valorA;
+
+
+    cout << "LABORATORIO 4 - ÁRBOLES BINARIOS" << endl;
+    cout << "============================================" << endl;
+    //A continuacion se hace un do con el switch y case para hacer el menu
+
+    do{
+
+        cout<<"\n --- Menú Arbol Binario de Busqueda ---"<<endl;
+        cout<<"1. Insertar Numero"<<endl;
+        cout<<"2. Modificar Numero"<<endl;
+        cout<<"3. Eliminar numero"<<endl;
+        cout<<"4. Mostrar recorridos"<<endl;
+        cout<<"5. Generar grafo"<<endl;
+        cout<<"0. Salir"<<endl;
+        cout<<"Seleccione una opcion: ";
         cin>>opcion;
-        cin.ignore();
 
-        switch (opcion){
-            case 1:
-                cout<<"Nombre: ";
-                getline(cin,nombre);
-                cout<<"Carrera: ";
-                getline(cin,carrera);
-                cout<<"Promedio: ";
-                cin>> promedio;
-                cin.ignore();
-                insertarCola(cola,nombre,carrera,promedio);
+        switch(opcion){
+            case 1: 
+                cout<<"Ingrese el numero a insertar: ";
+                cin>>valor;
+                raiz=insertarNum(raiz,valor);
                 break;
             case 2:
-                if(eliminarCola(cola,nombre,carrera,promedio)){
-                    cout<<"Se ha eliminado la cola: "<< nombre << " | "<<carrera<<" | Promedio: "<< promedio <<endl;
-                } else {
-                    cout<<"La cola está vacia.\n";
-                }
+                cout<<"Ingrese el numero a modificar: ";
+                cin>>valorA;
+                cout<<"Ingrese el nuevo valor: ";
+                cin>>valor;
+                raiz=modificar(raiz,valorA,valor);
                 break;
-            case 3: 
-                mostrarCola(cola);
+            case 3:
+                cout<<"Ingrese numero a eliminar: ";
+                cin>>valor;
+                raiz=removerNum(raiz,valor);
                 break;
             case 4:
-                cout<<"Ingrese la posicion a borrar: ";
-                cin>> pos;
-                if (borrarPosicion(cola,pos)){
-                    cout<<"Se borró el estudiante que se encontraba en la posicion: "<<pos<<endl;
+                if(raiz==nullptr){
+                    cout<<"El arbol está vacio."<<endl;
 
                 } else {
-                    cout<<"Posicion invalida :c"<<endl;
+                    cout<<"Preorden: ";
+                    preOrden(raiz);
+                    cout<< "\n";
 
+                    cout<<"Inorden: ";
+                    inOrden(raiz);
+                    cout<<"\n";
+
+                    cout<<"Posorden: ";
+                    posOrden(raiz);
+                    cout<<"\n";
                 }
                 break;
             case 5:
-                if (colaVacia(cola)){
-                    cout<<"La cola ta vacia. "<<endl;
+                if (raiz==nullptr){
+                    cout<<"El arbol está vacio. Inserte numeros antes de volver a intentarlo"<<endl;
 
                 } else {
-                    cout<<"La cola no ta vacia"<<endl;
+                    generarGrafo(raiz);
                 }
                 break;
-            case 6:
-                cout<<"Saliendo"<<endl;
+            case 0:
+                cout<<"Saliendo del programa"<<endl;
+                liberarMemoria(raiz); //Se libera memoria al momento de salir del programa
+                raiz= nullptr; //Esto según lo que investigué sirve para evitar algo llamado dangling pointer
                 break;
-            default: //Hace 5 minutos descubrí cual era la utilidad del default
-                cout<<"Opcion invalida";
+            default:
+            cout<<"Opcion no valida. Intente nuevamente"<<endl;
+
+
+
         }
-    } while (opcion!=6);
-    //Esto libera memoria
-    while(!colaVacia(cola)){
-        string nombre, carrera;
-        float promedio;
-        eliminarCola(cola,nombre,carrera, promedio); //Al finalizar el menú se eliminan todos los nodos
-    }
-    delete cola;
-    
-    
+
+    } while(opcion != 0);
+
 
     return 0;
 }
